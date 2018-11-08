8a2f7d8e815b3485222bed85ebeb82ee963f5866
Anthony_Bloch <ajgbloch@gmail.com>
2018-11-08 15:55:14 -0600

    changes made

---
+++ RTOS_demo.X/RTOS_Anthony.X/src/new_file.h
@@ -0,0 +1,17 @@
+
+#ifndef TASK_BLINKY_H
+#define	TASK_BLINKY_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+    extern void taskBlinky_init_new(void);
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* TASK_BLINKY_H */
+
